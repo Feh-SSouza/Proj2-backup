91a08ed49e4164a027065d0de14b2826a6116904
Felipe Sposito <felipesposito@hotmail.com.br>
2024-03-07 09:01:34 -0300

    backup-07/03/24-9am

--- app.py
+++ app.py
@@ -3,15 +3,21 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_zergo = Restaurante('Zergo', 'Hamburgeria')
-bebida_suco = Bebida('Suco de Laranja', 7, 'Tamanho grande')
-prato_refeicao = Prato('Burger', 18, '1x Carne, 1x Queijo, 1x Molho-Casa')
+bebida_suco = Bebida('Suco de Laranja', 7.90, 'Tamanho grande')
+bebida_suco.aplicar_desconto()
+
+prato_refeicao = Prato('Burger', 18.90, '1x Carne, 1x Queijo, 1x Molho-Casa')
+prato_refeicao.aplicar_desconto()
+
+restaurante_zergo.adicionar_cardapio(bebida_suco)
+restaurante_zergo.adicionar_cardapio(prato_refeicao)
+
 restaurante_place = Restaurante('Place', 'Pizzaria')
 restaurante_italy = Restaurante('Italy', 'Italiana')
 
 def main():
     '''Chama o método "listar_restaurantes" que lista os restaurantes.'''
-    print(bebida_suco)
-    print(prato_refeicao)
+    restaurante_zergo.exibir_cardapio
 
 if __name__ == '__main__': 
     main()
